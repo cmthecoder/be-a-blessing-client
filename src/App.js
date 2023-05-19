1d1fe6746c08a5855f438c84a922484966aed1f3
Chris Mems <christopher.mems@.com>
2023-05-19 14:49:10 -0500

    structure the navbar for both small and large screens

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Chat from "./pages/Chat";
+import Community from "./pages/Community";
+import Profile from "./pages/Profile";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/chat' element={<Chat />}/>
+        <Route path='/community' element={<Community />}/>
+        <Route path='/profile' element={<Profile />}/>
+      </Routes>
+      <Footer />
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,74 @@
-import React from 'react'
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import MenuIcon from '@mui/icons-material/Menu';
+import ClearIcon from '@mui/icons-material/Clear';
 
 const NavBar = () => {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
+
   return (
-    <div>NavBar</div>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#1E1F26] text-gray-300'>
+      <div>
+        <Link to="home" className='cursor-pointer'>
+          Logo
+          {/* <img src={Logo} alt="Logo" style={{ width: '65px'}}/> */}
+        </Link>
+      </div>
+        {/* Menu */}
+        <ul className='hidden md:flex gap-2'>
+          <li>
+            <Link to="/home">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/community">
+              Community
+            </Link>
+          </li>
+          <li>
+            <Link to="/chat">
+              Chat
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact">
+              Contact
+            </Link>
+          </li>
+          {/* Login and signup button */}
+        </ul>
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <MenuIcon /> : <ClearIcon />}
+      </div>
+
+      {/* Mobile menu */}
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#283655] flex flex-col justify-center items-center'}>
+        <li className='py-6 text-4xl'>
+            <Link onClick={handleClick} to="home">
+              Home
+            </Link>
+          </li>
+          <li className='py-6 text-4xl'>
+            <Link onClick={handleClick} to="community">
+              Community
+            </Link>
+          </li>
+          <li className='py-6 text-4xl'>
+            <Link onClick={handleClick} to="Chat">
+              Chat
+            </Link>
+          </li>
+          <li className='py-6 text-4xl'>
+            <Link onClick={handleClick} to="contact">
+              Contact
+            </Link>
+          </li>
+      </ul>
+    </div>
   )
 }
 
